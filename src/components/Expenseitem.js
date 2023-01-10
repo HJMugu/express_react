08ee711143ea2f77da221b22ae9685030ff674e1
Henri <henri-joel.mugu@voco.ee>
2023-01-10 11:30:48 +0200

    use javaScript logic inside component

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -3,11 +3,16 @@ import "./Expenseitem.css"
 
 
 function Expenseitem(props) {
-
-
+    const month = props.expenseData.date.toLocaleString('en-US', {month: "long"})
+    const day = props.expenseData.date.toLocaleString("en-US", {day: "2-digit"})
+    const year = props.expenseData.date.getFullYear
     return (
         <div className="expense-item">
-        <div>{props.expenseData.date.toDateString()}</div>
+        <div>
+            <div>{month}</div>
+            <div>{day}</div>
+            <div>{year}</div>
+        </div>
             <div className="expense-item__description">
                 <h2>{props.expenseData.title}</h2>
                 <div className="expense-item__price">{props.expenseData.price}</div>
