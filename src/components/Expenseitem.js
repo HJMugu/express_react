6f1c8080405646b0ed70ecfb7afe101470cd10b1
Henri <henri-joel.mugu@voco.ee>
2023-01-10 11:38:27 +0200

    split one component into several smaller ones

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,18 +1,12 @@
 //STIILIFAIL IMPORT
 import "./Expenseitem.css"
-
+import "./ExpenseDate"
 
 function Expenseitem(props) {
-    const month = props.expenseData.date.toLocaleString('en-US', {month: "long"})
-    const day = props.expenseData.date.toLocaleString("en-US", {day: "2-digit"})
-    const year = props.expenseData.date.getFullYear
+
     return (
         <div className="expense-item">
-        <div>
-            <div>{month}</div>
-            <div>{day}</div>
-            <div>{year}</div>
-        </div>
+            <expenseDate date={props.expenseData.date}></expenseDate>
             <div className="expense-item__description">
                 <h2>{props.expenseData.title}</h2>
                 <div className="expense-item__price">{props.expenseData.price}</div>
