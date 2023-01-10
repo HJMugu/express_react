1320b4c9c4ce6fb3a8e1cdf925274dd82aa87afc
Henri <henri-joel.mugu@voco.ee>
2023-01-10 09:55:22 +0200

    first, gitignore

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,13 @@
+function Expenseitem() {
+    return (
+        <div>
+        <div>Date</div>
+            <div>
+                <h2>Titles</h2>
+                <div>Amount</div>
+            </div>
+        </div>
+    )
+}
+
+export default Expenseitem
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+//see fail paneb App.js t66le, app.jsis toimuvad minu asjad
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
